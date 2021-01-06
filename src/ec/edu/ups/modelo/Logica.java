ad644b82dd8dbb5e639193b19f8d8d86bf546671
luisorellana31 <luisorellana31@yahoo.com>
2021-01-05 22:04:24 -0500

    Proyecto Base

---
+++ src/ec/edu/ups/modelo/Logica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.ups.modelo;
+
+/**
+ *
+ * @author Luis Orellana
+ */
+public class Logica {
+    public String metodoIntegrante1(){
+        
+        return "integrante 1";
+    }
+
+    public String metodoIntegrante2(){
+
+        retunr "integrante 2"
+    }
+}
